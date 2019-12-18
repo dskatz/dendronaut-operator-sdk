f267abd1e96c761c5868ecbfa1ddd86f46982cbb
David Katz <david.katz@mongodb.com>
2019-12-18 11:23:17 -0500

    Adding autogen controller and api.

---
+++ pkg/apis/dendronaut/group.go
@@ -0,0 +1,6 @@
+// Package dendronaut contains dendronaut API versions.
+//
+// This file ensures Go source parsers acknowledge the dendronaut package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package dendronaut
